b9c8cfe969f8297efa95021d83633131c48bc5e7
Franz Fernandez <franz.fernandez@doxatalent.com>
2024-12-18 21:16:15 -0500

    add db_name into Models and credentials.json

--- Models/Database.php
+++ Models/Database.php
@@ -3,18 +3,20 @@ class Database{
     private $host;
     private $user;
     private $password;
+    private $db_name;
     private $connection;
 
-    public function __construct($host, $user, $password) {
+    public function __construct($host, $user, $password, $db_name) {
         $this->host = $host;
         $this->user = $user;
         $this->password = $password;
+        $this->db_name = $db_name;
     }
 
     // metodo para conectar a la DB
     public function connect() {
 
-        $this->connection = new mysqli($this->host, $this->user, $this->password);
+        $this->connection = new mysqli($this->host, $this->user, $this->password, $this->db_name);
 
         
         if ($this->connection->connect_error) {
